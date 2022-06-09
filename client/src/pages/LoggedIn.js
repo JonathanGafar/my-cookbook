888180e7210b822d77fe63aa3c434bf8ddb86d41
JonathanGafar <jkgafar@gmail.com>
2022-06-08 22:31:40 -0400

    Created large-screen layout for LoggedIn.js
    The layout for a large screen will be a vertical navbar on the left side, spanning the entire height of the screen. There will be navigation buttons in the top right corner of the screen,
    and the recipe content will be below those navigation icons, and to the right of the navbar.
    
    This layout was achieved using a grid. I coloured the grid items so that I can see where each individual section is. However, I have already rendered the vertical navbar. Note that for a mobile screen the navbar will be at the top and will be horizontal. It will contain the navigation buttons in some type of menu, and the content will be below that. That layout will be created later.

--- client/src/pages/LoggedIn.js
+++ client/src/pages/LoggedIn.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {Grid, GridItem} from '@chakra-ui/react';
 
 import VNavbar from '../shared/VNavbar';
 
 export default function LoggedIn() {
 	return (
-		<VNavbar />
+		<Grid
+			h='100vh'
+			templateRows='5rem auto'
+			templateColumns='16rem auto'
+		>
+			<GridItem colStart='1' colEnd='2' rowStart='1' rowEnd='-1'>
+				<VNavbar />
+			</GridItem>
+			<GridItem bg='green' colStart='2' colEnd='-1' rowStart='1' rowEnd='2'>
+			</GridItem>
+			<GridItem bg='blue' colStart='2' colEnd='-1' rowStart='2' rowEnd='-1'>
+			</GridItem>
+		</Grid>
 	);
 }
