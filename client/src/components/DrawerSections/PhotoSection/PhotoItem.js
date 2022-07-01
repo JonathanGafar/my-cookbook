4955f860deb52e83421ca821e769becc827d69ff
JonathanGafar <jkgafar@gmail.com>
2022-07-01 18:03:41 -0400

    Changed PhotoItem.js
    I made it so that when the user clicks delete on a photo, an alert modal pops up asking them if they're sure that they want to delete the photo. I also made the empty photo pictures smaller on mobile.

--- client/src/components/DrawerSections/PhotoSection/PhotoItem.js
+++ client/src/components/DrawerSections/PhotoSection/PhotoItem.js
@@ -1,5 +1,19 @@
 import React, {useRef} from 'react';
-import {VStack, HStack, Image, Input} from '@chakra-ui/react';
+import {
+	VStack,
+	HStack,
+	Image,
+	Input,
+	Button,
+	AlertDialog,
+	AlertDialogHeader,
+	AlertDialogBody,
+	AlertDialogFooter,
+	AlertDialogOverlay,
+	AlertDialogContent,
+	useToast,
+	useDisclosure
+} from '@chakra-ui/react';
 import {BiPhotoAlbum} from 'react-icons/bi';
 import {FaTrash} from 'react-icons/fa';
 import {useSelector, useDispatch} from 'react-redux';
@@ -11,7 +25,11 @@ export default function PhotoItem(props) {
 	const photo = useSelector(state => state.photos.photos[props.photoNum]);
 	const dispatch = useDispatch();
 
+	const toast = useToast();
+	const {isOpen, onOpen, onClose} = useDisclosure();
+
 	const photoRef = useRef();
+	const cancelRef = useRef();
 
 	function onAddPhotoClick() {
 		photoRef.current.click();
@@ -19,13 +37,26 @@ export default function PhotoItem(props) {
 
 	function onDeletePhotoClick() {
 		dispatch(deletePhoto(props.photoNum));
+		onClose();
 	}
 
 	function photoInputOnChange() {
-		dispatch(addPhoto({
-			photoNum: props.photoNum,
-			photo: photoRef.current.files[0]
-		}));
+		if (photoRef.current.files[0].size < 5000000) {
+			dispatch(addPhoto({
+				photoNum: props.photoNum,
+				photo: URL.createObjectURL(photoRef.current.files[0])
+			}));
+		} else {
+			return toast({
+				title: 'File size is too big',
+				description:
+					'The file size limit is 5MB. Please re-save the photo in a different' +
+						' format if necessary.',
+				status: 'error',
+				duration: 6000,
+				isClosable: true
+			});
+		}
 	}
 
 	return (
@@ -33,14 +64,14 @@ export default function PhotoItem(props) {
 			{!photo &&
 				<Image
 					src={emptyPhoto}
-					boxSize={{base: '8rem', smd: '10rem', lg: '10rem'}}
+					boxSize={{base: '6rem', smd: '10rem', lg: '10rem'}}
 					objectFit='cover'
 					mb='0.5rem'
 					borderRadius='xl'
 				/>}
 			{photo &&
 				<Image
-					src={URL.createObjectURL(photo)}
+					src={photo}
 					boxSize={{base: '8rem', smd: '10rem', lg: '10rem'}}
 					objectFit='cover'
 					mb='0.5rem'
@@ -57,13 +88,42 @@ export default function PhotoItem(props) {
 					className='drawer-section-button'
 					aria-label='Delete ingredient'
 					size='1.1rem'
-					onClick={onDeletePhotoClick}
+					onClick={() => { if (photo) onOpen(); }}
 				/>
+				<AlertDialog
+					isOpen={isOpen}
+					leastDestructiveRef={cancelRef}
+					onClose={onClose}
+				>
+					<AlertDialogOverlay>
+						<AlertDialogContent w={{base: '90%', smd: 'auto'}}>
+							<AlertDialogHeader fontSize='lg' fontWeight='bold'>
+              					Delete Photo
+							</AlertDialogHeader>
+
+							<AlertDialogBody>
+              					Are you sure you want to delete this photo?
+							</AlertDialogBody>
+
+							<AlertDialogFooter>
+								<Button ref={cancelRef} onClick={onClose}>
+                					Cancel
+								</Button>
+								<Button
+									colorScheme='red'
+									onClick={onDeletePhotoClick}
+									ml={3}>
+                					Delete
+								</Button>
+							</AlertDialogFooter>
+						</AlertDialogContent>
+					</AlertDialogOverlay>
+				</AlertDialog>
 			</HStack>
 			<Input
 				ref={photoRef}
 				type='file'
-				accept='.png, .jpg, .jpeg'
+				accept='.png, .jpg, .jpeg, .svg'
 				display='none'
 				onChange={photoInputOnChange}
 			/>
