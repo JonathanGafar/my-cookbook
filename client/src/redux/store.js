24747d121cfec7d4c5910d1eb64263b9a09c4770
JonathanGafar <jkgafar@gmail.com>
2022-07-01 18:49:26 -0400

    Added a comment to explain the need for redux

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -8,6 +8,10 @@ import recipeStepsReducer from
 import photosReducer from
 	'../components/DrawerSections/PhotoSection/photosSlice';
 
+/* Stores the recipe in client state while the user is creating a recipe.
+This allows the user to close the create recipe modal, continue with something
+else, and pick up where they left off. Ultimately, the recipe will be saved in
+server state once the user clicks save. */
 export default configureStore({
 	reducer: {
 		description: descriptionReducer,
