729519dd96902e16e998ed38a8b030f6b66fc2f7
JonathanGafar <jkgafar@gmail.com>
2022-07-05 00:14:17 -0400

    Connected the app to mongodb

--- server/mongoose/mongoose.js
+++ server/mongoose/mongoose.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const User = require('./models/UserModel');
+require('dotenv').config();
 
 const connectionUrl = 	process.env.NODE_ENV === 'PRODUCTION' ?
 	process.env.PRODUCTION_DB_STRING : process.env.DEV_DB_STRING;
