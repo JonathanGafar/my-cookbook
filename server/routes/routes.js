9100c8562a79003e0c9b3b7cc646210a4725110c
JonathanGafar <jkgafar@gmail.com>
2022-07-11 21:52:10 -0400

    Accessing protected routes
    I created a middleware that will be ran whenever the user tries to access a protected route. If req.user exists, meaning the user is logged in and there is a valid session, then we continue onto the next middleware function. Otherwise, an error message is returned and the request-response cycle is ended.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,5 +5,8 @@ const authControllers = require('../controllers/authControllers');
 
 router.post('/users', authControllers.signupUser);
 router.post('/login', authControllers.loginUser);
+router.get('/users/:id', authControllers.confirmUser, function(req, res, next) {
+	return next();
+});
 
 module.exports = router;
