a8aedad5cc8389f468233942b5bbfc0eb0a75039
JonathanGafar <jkgafar@gmail.com>
2022-07-22 23:12:27 -0400

    S3 setup
    In my S3 console, I created the bucket, a user, and a policy. I created an S3Config.js file in a new folder, called S3. This just allows the app to access the S3 bucket. I also installed multer, multer-s3, and aws-sdk to assist in this task.

---
+++ server/S3/S3Config.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const fs = require('fs');
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+
+aws.config.update({
+	secretAccessKey: process.env.S3_ACCESS_KEY,
+	accessKeyId: process.env.S3_ACCESS_KEY_ID,
+	region: process.env.S3_REGION
+});
+
+const s3 = new aws.S3();
+
+const fileFilter = (req, file, cb) => {
+	if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || 
+		file.mimetype === 'image/png'
+	) {
+		cb(null, true);
+	} else {
+		cb(new Error('Invalid file type, only JPEG, JPG, and PNG is allowed'),
+			false);
+	}
+};
+
+const upload = multer({
+	fileFilter,
+	storage: multerS3({
+		acl: 'bucket-owner-read',
+		s3,
+		bucket: process.env.S3_BUCKET_NAME,
+		metadata: function(req, file, cb) {
+			cb(null, {fieldName: 'TESTING_METADATA'});
+		},
+		key: function(req, file, cb) {
+			cb(null, Date.now().toString());
+		},
+	}),
+});
+
+module.exports = upload;
